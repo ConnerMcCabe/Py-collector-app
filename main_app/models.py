9b780a8127160d790eb7da0f6186ce45f7f5649b
Admin <Admin@MacBook-Pro-2.local>
2019-09-16 11:57:16 -0500

    working Oauth

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,8 @@ class Rat(models.Model):
     breed = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     age = models.IntegerField()
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name}'
